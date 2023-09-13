96a275e9615aa6d99689227458290e640ee33134
sanjaybtp91 <144901760+sanjaybtp91@users.noreply.github.com>
2023-09-13 17:45:33 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "sanjay@gmail.com"
+var accountpassword = "123456"
+accountcity = "jaipur"
+
+
+
+console.table([accountId,accountEmail,accountpassword,accountcity])
