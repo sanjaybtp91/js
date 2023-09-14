052675c8b738c249b2ce67e0fc8452617ec393fa
sanjaybtp91 <144901760+sanjaybtp91@users.noreply.github.com>
2023-09-14 05:40:45 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert("3+3")
+
+// console.log(alert);
+console.log("sanjay")
+
+
+
+let name = ("sanjay")
+let age = 18
+let islengthIn = false
+
+
+// number  =>2 to 53
+// bigint for large number
+// string => ""
+// boolean => true / false
+// null => stand alone value
+// undefined => (value assign nhi huyi hai)
+// symbol => unique 
+
+
+
+
+// object 
+
+console.log(typeof "sanjay")
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
